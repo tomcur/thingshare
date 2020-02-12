65f7996cb20a0535e54d0681b3f0fc14846611e1
Thomas Churchman <thomas@kepow.org>
2020-02-12 01:45:12 +0100

    Run nixfmt.

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-with import <nixpkgs> {};
+with import <nixpkgs> { };
 
 stdenv.mkDerivation rec {
   name = "thingshare-${version}";
@@ -6,20 +6,44 @@ stdenv.mkDerivation rec {
 
   src = ./.;
   nativeBuildInputs = [ makeWrapper ];
-  
+
   phases = "installPhase";
 
   installPhase = ''
     mkdir -p $out/bin
     cp ${src}/bin/* $out/bin/
     chmod +x $out/bin/*
-    wrapProgram $out/bin/thingshare_clipboard --prefix PATH : ${lib.makeBinPath [ pkgs.libnotify pkgs.coreutils pkgs.gnugrep pkgs.curl pkgs.jq pkgs.file pkgs.gawk pkgs.xxd pkgs.xclip ]}
-    wrapProgram $out/bin/thingshare_screenshot --prefix PATH : ${lib.makeBinPath [ pkgs.libnotify pkgs.scrot pkgs.flameshot pkgs.coreutils pkgs.gnugrep pkgs.curl pkgs.jq pkgs.file pkgs.gawk pkgs.xxd ]}
-    wrapProgram $out/bin/thingshare_init --prefix PATH : ${lib.makeBinPath [ pkgs.flameshot ]}
+    wrapProgram $out/bin/thingshare_clipboard --prefix PATH : ${
+      lib.makeBinPath [
+        pkgs.libnotify
+        pkgs.coreutils
+        pkgs.gnugrep
+        pkgs.curl
+        pkgs.jq
+        pkgs.file
+        pkgs.gawk
+        pkgs.xxd
+        pkgs.xclip
+      ]
+    }
+    wrapProgram $out/bin/thingshare_screenshot --prefix PATH : ${
+      lib.makeBinPath [
+        pkgs.libnotify
+        pkgs.scrot
+        pkgs.flameshot
+        pkgs.coreutils
+        pkgs.gnugrep
+        pkgs.curl
+        pkgs.jq
+        pkgs.file
+        pkgs.gawk
+        pkgs.xxd
+      ]
+    }
+    wrapProgram $out/bin/thingshare_init --prefix PATH : ${
+      lib.makeBinPath [ pkgs.flameshot ]
+    }
   '';
 
-
-  meta = {
-    description = "Share screenshots and your clipboard.";
-  };
+  meta = { description = "Share screenshots and your clipboard."; };
 }
